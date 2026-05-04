4168085bb9a4ccefee5501004ac1fcd6957fc00f
phil <phil_lam@sfu.ca>
2026-05-03 21:01:46 -0700

    Update ideas.ts

--- website/src/ideas.ts
+++ website/src/ideas.ts
@@ -10,7 +10,7 @@ export const ideas: Idea[] = [
     status: 'Dominic Fike - White Keys',
     topic: 'Your Friends & Neighbors (Season 2)',
     details: {
-      description: 'I’m a Business student at SFU Beedie, specializing in Management Information Systems and Operations, while concurrently completing a Certificate in Business Analytics and Decision Making. I’m particularly interested in how data can be leveraged to inform strategy and drive smarter decision-making within financial institutions. Outside of academics, you’ll usually find me playing Valorant, scrolling TikTok, watching anime, or enjoying an iced cap!',
+      description: 'I’m a Business student at SFU Beedie, specializing in Management Information Systems and Operations Management, while concurrently completing a Certificate in Business Analytics and Decision Making. I’m particularly interested in how data can be leveraged to inform strategy and drive smarter decision-making within financial institutions. Outside of academics, you’ll usually find me playing Valorant, scrolling TikTok, watching anime, or enjoying an iced cap!',
       thoughts: '',
       relevance: ''
     },
